31228c08bc01460ab435ae5ba3b9f3047143cc7b
Joah Gerstenberg <me@joahg.com>
2014-08-21 09:08:23 -0500

    now without jquery.

--- magic.js
+++ magic.js
@@ -1,7 +1,21 @@
-$('img').each(function() {
-    $(this).attr('src', 'http://placebeard.it/' + $(this).width().toString() + 'x' + $(this).height().toString() + '/notag?cacheclear='+Math.random().toString())
-})
+var w_bgs = Array.prototype.filter.call(document.querySelectorAll('*'), function(node) {
+	if (this.currentStyle) { 
+		return this.currentStyle['backgroundImage'] !== 'none';
+	} else if (window.getComputedStyle) { 
+		return document.defaultView.getComputedStyle(node,null).getPropertyValue('background-image') !== 'none';
+	}
+});
 
-$('*').filter(function() {if (this.currentStyle) { return this.currentStyle['backgroundImage'] !== 'none';} else if (window.getComputedStyle) { return document.defaultView.getComputedStyle(this,null).getPropertyValue('background-image') !== 'none';}}).each(function() {
-    $(this).css('background-image', 'url("http://placebeard.it/' + $(this).width().toString() + 'x' + $(this).height().toString() + '/notag?cacheclear='+Math.random().toString() + '")')
-})
+for (i in w_bgs) {
+	var bg = w_bgs[i];
+	bg.style.backgroundImage = 'url("http://placebeard.it/' + bg.offsetWidth.toString() + 'x' + bg.offsetHeight .toString() + '/notag?cacheclear='+Math.random().toString() + '")'
+}
+
+var imgs = document.querySelectorAll('img');
+
+for (i in imgs) {
+	var img = imgs[i];
+	if (img && img.src) {
+		img.src = 'http://placebeard.it/' + img.offsetWidth.toString() + 'x' + img.offsetHeight.toString() + '/notag?cacheclear=' + Math.random().toString();
+	}
+}
